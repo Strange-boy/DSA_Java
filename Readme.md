f5c1cdbde12f162139d020311ff78d827cfa4423
Strange-boy <joel.panvel@gmail.com>
2024-05-12 19:19:04 +0530

    Remove last node in Linked List

--- Readme.md
+++ Readme.md
@@ -59,7 +59,7 @@
 | 4 | Get Value in the List |  | [pep](https://www.youtube.com/watch?v=MGwnkGap5s8&list=PL-Jc9J83PIiF5VZmktfqW6WVU1pxBF6l_&index=10) |[pep](https://www.youtube.com/watch?v=MGwnkGap5s8&list=PL-Jc9J83PIiF5VZmktfqW6WVU1pxBF6l_&index=10) | [github](https://github.com/Strange-boy/DSA_Java/blob/main/src/GetValueInList.java)|
 | 5 | Add First in the List |  | [pep](https://www.youtube.com/watch?v=qsz2QlQpFg4&list=PL-Jc9J83PIiF5VZmktfqW6WVU1pxBF6l_&index=11) | [pep](https://www.youtube.com/watch?v=DfP-tb68t20&list=PL-Jc9J83PIiF5VZmktfqW6WVU1pxBF6l_&index=12) |[github](https://github.com/Strange-boy/DSA_Java/blob/main/src/AddFirstInList.java) |
 | 6 | Add at Index in List |  | [pep](https://www.youtube.com/watch?v=ObCDzE-eAgA&list=PL-Jc9J83PIiF5VZmktfqW6WVU1pxBF6l_&index=13) | [pep](https://www.youtube.com/watch?v=n9E27Z4zjpk&list=PL-Jc9J83PIiF5VZmktfqW6WVU1pxBF6l_&index=14) | [github](https://github.com/Strange-boy/DSA_Java/blob/main/src/AddAtIndexList.java) |
-|  |  |  |  | | |
+| 7 |  Remove last Node  |  | [pep](https://www.youtube.com/watch?v=67s3iHctemA&list=PL-Jc9J83PIiF5VZmktfqW6WVU1pxBF6l_&index=15) | [pep](https://www.youtube.com/watch?v=WTG87ZABrtY&list=PL-Jc9J83PIiF5VZmktfqW6WVU1pxBF6l_&index=16) |[github](https://github.com/Strange-boy/DSA_Java/blob/main/src/RemoveLastList.java) |
 |  |  |  |  | | |
 |  |  |  |  | | |
 |  |  |  |  | | |
---
+++ src/RemoveLastList.java
@@ -0,0 +1,124 @@
+public class RemoveLastList {
+    //in order to create the node
+    public static class Node{
+        int data;
+        Node next;
+    }
+
+    //in order to declare our own linked list class
+    public static class LinkedList{
+        Node head;
+        Node tail;
+        int size;
+
+        public void addLast(int value){
+            //create the node
+            Node temp = new Node();
+            temp.data = value;
+            temp.next = null;
+
+            if(size == 0){
+                head = tail = temp;
+            } else {
+                tail.next = temp;
+                tail = temp;
+            }
+
+            size += 1;
+        }
+
+        //add the element at first
+        public void addFirst(int value){
+            //create the node
+            Node temp = new Node();
+            temp.data = value;
+            temp.next = null;
+
+            if(size == 0){
+                head = tail = temp;
+            } else {
+                temp.next = head;
+                head = temp;
+            }
+
+            size += 1;
+        }
+
+        //add at any index
+        public void addAt(int idx, int value){
+            if(idx < 0 || idx >= size){
+                System.out.println("Invalid index");
+            } else if(idx == 0){
+                addFirst(value);
+            } else if(idx == size - 1){
+                addLast(value);
+            } else {
+                Node newNode = new Node();
+                newNode.data = value;
+
+                //temp node for iteration
+                Node temp = head;
+
+                for(int currIdx = 0; currIdx < idx - 1; currIdx += 1){
+                    temp = temp.next;
+                }
+
+                //now in order to connect the new node
+                newNode.next = temp.next;
+                temp.next = newNode;
+
+                size += 1;
+            }
+        }
+
+        //in order to display the entire list
+        public void showElements(){
+            System.out.println("The Elements in the list are: ");
+            for(Node temp = head; temp != null; temp = temp.next){
+                System.out.println(temp.data);
+            }
+        }
+
+        //in order to remove the last element in the linked list
+        public void removeLast(){
+            if(size == 0){
+                System.out.println("The List is empty");
+            }else if(size == 1){
+                head = tail = null;
+                size -= 1;
+            } else {
+                Node temp = head;
+
+                for(int idx = 0; idx < size - 2; idx += 1){
+                    temp = temp.next;
+                }
+
+                temp.next = null;
+                tail = temp;
+                size -= 1;
+            }
+        }
+    }
+
+    //main function
+    public static void main(String[] args){
+        LinkedList list = new LinkedList();
+
+        list.removeLast();
+        list.addFirst(10);
+        list.addLast(20);
+        list.addAt(9, 90);
+        list.addAt(1, 90);
+        list.showElements();
+
+        list.addFirst(30);
+        list.addLast(100);
+        list.addAt(2, 9);
+        list.addAt(1, 8);
+        list.removeLast();
+        list.showElements();
+
+        list.removeLast();
+        list.showElements();
+    }
+}
